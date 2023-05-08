9505b89220bae5185cd800c080c8182f101ce04a
subin <github@subin.kr>
2023-05-08 18:51:37 +0900

    styled-components

--- src/context/Main.jsx
+++ src/context/Main.jsx
@@ -1,17 +1,22 @@
 import React, { useContext } from "react";
 import ThemeContext from "./ThemeContext";
+import { styled } from "styled-components";
+
+const Wrapper = styled.div`
+  background: #f1f1f1;
+  color: ${props => props.theme === 'light' ? 'blue' : 'red'};
+  display: flex;
+  justify-content: space-around;
+`
 
 function Main(props) {
   const {theme, toggleTheme} = useContext(ThemeContext);
 
   return (
-    <div style = {{
-      backgroundColor: theme === 'light' ? '#fff' : '#000',
-      color: theme === 'light' ? '#000' : '#fff'
-    }}>
+    <Wrapper theme={theme}>
       <h3>Main</h3>,
       <button onClick={toggleTheme}>Toogle Theme</button>
-    </div>
+    </Wrapper>
   );
 }
 
